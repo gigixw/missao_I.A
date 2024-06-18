d89ae823a0c3ada2e835409860bdcabbaa206f9a
Geovana <107410963+gigixw@users.noreply.github.com>
2024-06-18 09:47:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+    enunciado: "Em que lugar teve origem a palavra música?",
+    alternativas: ["Grécia", "China", "Brasil", "India"],
+    },
+    {
+    enunciado: "O teatro é uma linguagem de que arte?",
+    alternativas: ["Literárias", "Cênicas", "Visuais", "Musicais"],
+    },
+    {
+    enunciado: "Qual é um dos elementos que compõem a dança?",
+    alternativas: ["Contrapeso", "Espaço", "Ordem", "Transferência"], 
+    }
+    ];
+
+    let atual = 0;
+    let perguntaAtual;
+    
+    function mostraPergunta() {
+        perguntaAtual = perguntas [atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraAlternativas();
+    }
+    mostraPergunta();
+    function mostraAlternativas () {
+        for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document.createElement ("button");
+            botaoAlternativas.textContent = alternativa;
+            caixaAlternativas.appendChild(botaoAlternativas);
+        }
+
+    }
