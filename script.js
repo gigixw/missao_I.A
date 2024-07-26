cd315f9f58c73c4af12c4453317dfffc9c24ddb3
Geovana <107410963+gigixw@users.noreply.github.com>
2024-07-26 11:44:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,39 +7,69 @@ const textoResultado = document.querySelector('.texto-resultado');
 const perguntas = [
     {
     enunciado: "Em que lugar teve origem a palavra música?",
-    alternativas: ["Grécia", "China", "Brasil", "India"
-       {
-        texto: "Grécia",
-        afirmação: "Afirmação da alternativa 1"
-       }
-       
-
-
-
-
-
-
-
-
-
-
-
-],
-    },
-    
-    
-    
-    
-    
+    alternativas: [
+        {
+            texto: "China",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto: "Grécia",
+            afirmação: "Afirmação da alternativa 2"
+        },
+        {
+            texto: "Brasil",
+            afirmação: "Afirmação da alternativa 3"
+        },
+        {
+            texto: "India",
+            afirmação: "Afirmação da alternativa 4"
+        },
+    ]
+},
     
     {
-    enunciado: "O teatro é uma linguagem de que arte?",
-    alternativas: ["Literárias", "Cênicas", "Visuais", "Musicais"],
-    },
+        enunciado: "O teatro é uma linguagem de que arte?",
+    alternativas: [
+        {
+            texto: "Literárias",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto: "Cênicas",
+            afirmação: "Afirmação da alternativa 2"
+        },
+        {
+            texto: "Visuais",
+            afirmação: "Afirmação da alternativa 3"
+        },
+        {
+            texto: "Musicais",
+            afirmação: "Afirmação da alternativa 4"
+        },
+    ]
+},
+
     {
-    enunciado: "Qual é um dos elementos que compõem a dança?",
-    alternativas: ["Contrapeso", "Espaço", "Ordem", "Transferência"], 
-    }
+        enunciado: "Qual é um dos elementos que compõem a dança?",
+    alternativas: [
+        {
+            texto: "Contrapeso",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto: "Espaço",
+            afirmação: "Afirmação da alternativa 2"
+        },
+        {
+            texto: "Ordem",
+            afirmação: "Afirmação da alternativa 3"
+        },
+        {
+            texto: "Transferência",
+            afirmação: "Afirmação da alternativa 4"
+        },
+    ]
+},
     ];
 
     let atual = 0;
@@ -53,9 +83,13 @@ const perguntas = [
     mostraPergunta();
     function mostraAlternativas () {
         for (const alternativa of perguntaAtual.alternativas) {
-            const botaoAlternativas = document.createElement ("button");
-            botaoAlternativas.textContent = alternativa;
+            const botaoAlternativas = 
+            document.createElement ("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function () {
+               atual++;
+               mostraPergunta();
+            });
             caixaAlternativas.appendChild(botaoAlternativas);
         }
-
     }
